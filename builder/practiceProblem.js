dc8c4be40cc522b01ce3314c670bae79e73ef13d
Julia Meller Mendes Silva <julia.3123@alunosatc.edu.br>
2025-09-03 20:24:54 -0300

    exercicio builder

--- builder/practiceProblem.js
+++ builder/practiceProblem.js
@@ -1,28 +1,94 @@
 class Car {
-  constructor(brand, model, year, engine, color, gps) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.engine = engine;
-    this.color = color;
-    this.gps = gps;
+  constructor() {
+    this.brand = null;
+    this.model = null;
+    this.year = null;
+    this.engine = null;
+    this.color = null;
+    this.gps = false;
   }
 
   showDetails() {
-    console.log(
-      `${this.year} 
-      ${this.brand} 
-      ${this.model} 
-      - Motor: ${this.engine}, 
+    console.log(`
+      Carro:
+      Ano: ${this.year} 
+      Marca: ${this.brand} 
+      Modelo: ${this.model} 
+      Motor: ${this.engine}, 
       Cor: ${this.color}, 
       GPS: ${this.gps ? "Sim" : "Não"}`
     );
   }
 }
 
-// Cliente precisa lembrar a ordem e todos os parâmetros
-const car1 = new Car("Toyota", "Corolla", 2024, "2.0", "Preto", true);
-const car2 = new Car("Honda", "Civic", 2023, "1.5 Turbo", "Prata", false);
+class CarBuilder {
+  constructor() {
+    this.car = new Car();
+  }
 
-car1.showDetails();
-car2.showDetails();
+  setBrand(brand) {
+    this.car.brand = brand;
+    return this;
+  }
+
+  setModel(model) {
+    this.car.model = model;
+    return this;
+  }
+
+  setYear(year) {
+    this.car.year = year;
+    return this;
+  }
+
+  setEngine(engine) {
+    this.car.engine = engine;
+    return this;
+  }
+
+  setColor(color) {
+    this.car.color = color;
+    return this;
+  }
+
+  setGps(gps) {
+    this.car.gps = gps;
+    return this;
+  }
+
+  build() {
+    return this.car;
+  }
+}
+
+class CarDirector {
+  static buildWorkCar() {
+    return new CarBuilder()
+    .setBrand("Fiat")
+    .setModel("Uno")
+    .setYear(2010)
+    .setEngine("1.0")
+    .setColor("Branco")
+    .setGps(true)
+    .build();
+  }
+
+  static buildFamilyCar() {
+    return new CarBuilder()
+    .setBrand("Honda")
+    .setModel("Civic")
+    .setYear(2023)
+    .setEngine("1.5 Turbo")
+    .setColor("Prata")
+    .setGps(false)
+    .build();
+  }
+}
+
+const workCar = CarDirector.buildWorkCar();
+const familyCar = CarDirector.buildFamilyCar();
+const sportCar = new CarBuilder().setBrand("Ferrari").build();
+
+workCar.showDetails();
+familyCar.showDetails();
+sportCar.showDetails();
